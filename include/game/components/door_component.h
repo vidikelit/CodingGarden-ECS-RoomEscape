fc690b905eaf54f053d1303d23beeb798fb2b813
Vadim Lemmer <vidikelit@gmail.com>
2019-09-13 17:38:21 +0600

    [+]Добавлена генерация дверей

---
+++ include/game/components/door_component.h
@@ -0,0 +1,8 @@
+//
+// Created by vidik on 13.09.2019.
+//
+
+#ifndef ECS_DOOR_COMPONENT_H
+#define ECS_DOOR_COMPONENT_H
+
+#endif  // ECS_DOOR_COMPONENT_H
---
+++ include/game/system/game_door_system.h
@@ -0,0 +1,10 @@
+//
+// Created by vidik on 13.09.2019.
+//
+
+#ifndef ECS_GAME_DOOR_SYSTEM_H
+#define ECS_GAME_DOOR_SYSTEM_H
+
+class game_door_system {};
+
+#endif  // ECS_GAME_DOOR_SYSTEM_H
